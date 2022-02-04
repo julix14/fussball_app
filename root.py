7919ef9f9d78cb5ca362b0ff09b14f7f56fa7ccb
julius <julix.becker@gmail.com>
2022-02-04 16:27:04 +0100

    remove dublicates

--- root.py
+++
@@ -1,8 +0,0 @@
-from importlib import import_module
-
-
-import wettbewerbe_laden
-import tabellen_erhalten
-
-verbaende = wettbewerbe_laden.wettbewerb_laden()
-tabellen_erhalten.links_erzeugen(verbaende,"_2122","_1")
